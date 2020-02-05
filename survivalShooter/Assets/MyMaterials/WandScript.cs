71fe3cdcad33001ec48aa36bdb994f7c26659733
AlexTCheramie <alextcheramie@gmail.com>
2020-02-05 13:59:55 -0600

    inital commit

---
+++ survivalShooter/Assets/MyMaterials/WandScript.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WandScript : MonoBehaviour
+{
+    public Material mat;
+    private Material originalMat;
+    // Start is called before the first frame update
+    void Start()
+    {
+        originalMat = GetComponentInChildren<MeshRenderer>().material;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float mouseX = Input.mousePosition.x;
+        float mouseY = Input.mousePosition.y;
+        float screenWidth = Screen.width;
+        float screenHeight = Screen.height;
+
+        float mouseNormX = mouseX / screenWidth;
+        float mouseNormY = mouseY / screenHeight;
+        WandOrientation(mouseNormX, mouseNormY);
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            GetComponentInChildren<MeshRenderer>().material = mat;
+        }
+        if (Input.GetMouseButtonUp(0))
+        {
+            GetComponentInChildren<MeshRenderer>().material = originalMat;
+        }
+    }
+
+    void WandOrientation(float mouseNormX, float mouseNormY)
+    {
+        float xAngle = (-75f * (mouseNormY - 0.5f));
+        float yAngle = (75f * (mouseNormX - 0.5f));
+        Vector3 motion = new Vector3(xAngle, yAngle, 0f);
+        transform.localEulerAngles = motion;
+    }
+
+}
---
+++ survivalShooter/Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed = 6f;
+    Vector3 movement;
+    Animator anim;
+    Rigidbody playerRigidbody;
+    int floorMask;
+    float camRayLength = 100f;
+    public float turnSpeed = 100f;
+
+    private void Awake()
+    {
+        floorMask = LayerMask.GetMask("Floor");
+        anim = GetComponent<Animator>();
+        playerRigidbody = GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate()
+    {
+        float h = Input.GetAxisRaw("Horizontal");
+        float v = Input.GetAxisRaw("Vertical");
+
+        Move(h, v);
+        Turning(h);
+        Animating(h, v);
+
+    }
+
+    void Move(float h, float v)
+    {
+        //movement.Set(h, 0f, v);
+
+        movement = transform.forward * v;
+        movement = movement.normalized * speed * Time.deltaTime;
+        playerRigidbody.MovePosition(transform.position + movement);
+    }
+
+    void Turning(float h)
+    {
+        /*Ray camRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit floorHit;
+        if (Physics.Raycast(camRay, out floorHit, camRayLength, floorMask))
+        {
+            Vector3 playerToMouse = floorHit.point - transform.position;
+            playerToMouse.y = 0f;
+            Quaternion newRotation = Quaternion.LookRotation(playerToMouse);
+            playerRigidbody.MoveRotation(newRotation);
+        }*/
+
+
+        Vector3 eulerAngleVelocity = new Vector3(0f, h * turnSpeed, 0f);        //Create a new vector with h, multipled by how fast you want to turn around the y-axis
+        Quaternion deltaRotation = Quaternion.Euler(eulerAngleVelocity * Time.deltaTime);
+        playerRigidbody.MoveRotation(playerRigidbody.rotation * deltaRotation);
+
+    }
+
+    void Animating(float h, float v)
+    {
+        bool walking = (h != 0f || v != 0f);
+        anim.SetBool("IsWalking", walking);
+    }
+}
